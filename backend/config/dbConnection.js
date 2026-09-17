f32bbdcbc66e20cca105647e6aace127b2c4c0c7
Ajmal Fayiz <fayizjmal47@gmail.com>
2026-09-17 17:27:39 +0530

    predeploy

--- backend/config/dbConnection.js
+++ backend/config/dbConnection.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 
+
 const connectDB = async () => {
   try {
     const connection = await mongoose.connect(process.env.MONGO_URI);
--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,7 @@
+const dns = require("node:dns");
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
+
 require("dotenv").config();
 
 const express = require("express");
@@ -39,7 +43,7 @@ app.use((req, res) => {
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
